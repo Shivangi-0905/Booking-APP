d561e8851e9ea2bdea3182938a65d8dda9bca24c
Shivangi Upadhyay <153377473+Shivangi-0905@users.noreply.github.com>
2025-05-24 14:49:05 +0530

    Update DoctorContext.jsx

--- admin/src/context/DoctorContext.jsx
+++ admin/src/context/DoctorContext.jsx
@@ -7,7 +7,7 @@ import { toast } from "react-toastify";
 export const DoctorContext = createContext();
 
 const DoctorContextProvider = (props) => {
-  const backendUrl = import.meta.env.VITE_BACKEND_URL;
+  const backendUrl = "https://booking-app-backend-glgj.onrender.com";
 
   const [dToken, setDToken] = useState(
     localStorage.getItem("dToken") ? localStorage.getItem("dToken") : ""

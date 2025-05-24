ea46ae3a7e621948b8a089aec6a47024f1d5234f
Shivangi Upadhyay <153377473+Shivangi-0905@users.noreply.github.com>
2025-05-24 14:42:35 +0530

    Update AppContext.jsx

--- BookinApp/src/context/AppContext.jsx
+++ BookinApp/src/context/AppContext.jsx
@@ -6,7 +6,7 @@ export const AppContext = createContext();
 
 const AppContextProvider = (props) => {
   const currencySymbol = "$";
-  const backendUrl = import.meta.env.VITE_BACKEND_URL;
+  const backendUrl = "https://booking-app-backend-glgj.onrender.com";
 
   const [doctors, setDoctors] = useState([]);
   const [token, setToken] = useState(
